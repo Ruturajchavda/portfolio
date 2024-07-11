31077ac61535fe2420b54149868c8e7a0c799d8b
Ruturaj Chavda <ruturajchavda18@gmail.com>
2024-07-11 13:22:06 -0400

    Added experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
+    title: "Self Employed",
+    company: "Code and build something everyday",
+    duration: "(May 2024 - Present)"
+  },
+  {
+    id: 2,
     title: "Mobile Application Developer",
     company: "Learniphi Technology Pvt. Ltd.",
     duration: "(Apr 2019 - Nov 2022)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Engineer Intern",
     company: "Srashtasoft Technology",
     duration: "(Mar 2018 - Mar 2019)"
